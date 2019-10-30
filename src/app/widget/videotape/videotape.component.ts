fb3f29992824f798f070c91d7ce72d321fb502f4
zsqx <1257477428@qq.com>
2019-10-30 13:53:28 +0800

    sa

--- src/app/widget/videotape/videotape.component.ts
+++ src/app/widget/videotape/videotape.component.ts
@@ -1,34 +1,71 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { MediaCapture, CaptureError,MediaFile } from "@ionic-native/media-capture/ngx";
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { PageEffectService } from 'src/app/services/page-effect.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MediaCapture, CaptureError, MediaFile } from '@ionic-native/media-capture/ngx';
+import { ActionSheetOptions } from '@ionic/core';
 
 @Component({
-  selector: "app-videotape",
-  templateUrl: "./videotape.component.html",
-  styleUrls: ["./videotape.component.scss"]
+  selector: 'app-videotape',
+  templateUrl: './videotape.component.html',
+  styleUrls: ['./videotape.component.scss']
 })
 export class VideotapeComponent implements OnInit {
 
-  @Input() set videos(input:MediaFile[][]){
-      if(!!input) this.videos = input
+  @Input() set videos(input: MediaFile[][]) {
+      if (!!input) { this.videos = input; }
   }
 
-  @Output() onComplete:EventEmitter<MediaFile[][]> = new EventEmitter<MediaFile[][]>()
+  constructor(public mediaCapture: MediaCapture,
+              private fileChooser: FileChooser,
+              private ec: PageEffectService) {}
 
-  constructor(public mediaCapture: MediaCapture) {}
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onComplete: EventEmitter<MediaFile[][]> = new EventEmitter<MediaFile[][]>();
+
+  // tslint:disable-next-line: variable-name
+  _videos: MediaFile[][] = [
+
+  ];
   ngOnInit() {}
 
-  _videos: MediaFile[][] = [
-   
-  ]
-
   videotape() {
+    const option: ActionSheetOptions = {
+      header: '上传方式',
+      buttons: [{
+        text: '录像',
+        role: '',
+        icon: 'camera',
+        handler: () => {
+          this.tape();
+        }
+      }, {
+        text: '选择文件',
+        icon: 'folder',
+        handler: () => {
+          this.fileChoose();
+        }
+      }],
+      mode: 'ios'
+    };
+    this.ec.showActionSheet(option);
+  }
+
+  fileChoose() {
+    this.fileChooser.open()
+        .then(uri => console.log(uri))
+        .catch(e => console.log(e));
+  }
+
+  tape() {
     this.mediaCapture.captureVideo({ limit: 1, quality: 30 }).then(
       (mediaFiles: any[]) => {
         console.log(mediaFiles);
         this._videos.push(mediaFiles);
-        this.onComplete.emit(this._videos)
+        this.onComplete.emit(this._videos);
       },
       (err: CaptureError) => {}
     );
   }
+
+
 }
